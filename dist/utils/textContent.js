3b6755ac34fdd0a5b39768be8f51596e860c2b64
snygghugo <karlhugodahlgren@gmail.com>
2023-08-04 12:55:38 +0000

    More gitignore cleanup

--- dist/utils/textContent.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.botMessages = void 0;
-exports.botMessages = {
-    rolePrefTitle: 'Your Dota 2 Role Preferences',
-    rolePrefFieldTitle: 'Please click the reactions below to indicate your role preferences',
-    rolePrefFieldValue: 'Please select your most preferred role',
-};
