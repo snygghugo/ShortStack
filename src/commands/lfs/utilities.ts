97d7f9bdb5c9324d59de9c1890efa93ba822b13d
snygghugo <karlhugodahlgren@gmail.com>
2023-08-05 17:56:33 +0000

    Small copy change

--- src/commands/lfs/utilities.ts
+++ src/commands/lfs/utilities.ts
@@ -135,7 +135,7 @@ export const pingMessage = async (
   for (let player of readyArray) {
     if (!player.ready) {
       const gentleReminder = await partyThread.send(
-        `${player.gamer.toString()} we're waiting!}`
+        `${player.gamer.toString()} hurry up, we're waiting!}`
       );
       reminders.push(gentleReminder);
     }
