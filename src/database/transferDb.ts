5ed67c18ad2186e50fb0cb4a683d729b354c0f94
snygghugo <karlhugodahlgren@gmail.com>
2023-08-12 16:10:59 +0200

    More debug logs

--- src/database/transferDb.ts
+++ src/database/transferDb.ts
@@ -2,8 +2,10 @@ import settings from '../../settings.json';
 import { updateUserPrefs } from './db';
 
 export const transferDb = async () => {
+  console.log('this is settings', settings);
   for (const guild in settings) {
     const guildToCheck = settings[guild as keyof typeof settings];
+    console.log('this is guildtocheck', guildToCheck);
     for (const key in guildToCheck) {
       const player = guildToCheck[key as keyof typeof guildToCheck];
       console.log('this is player', player);
